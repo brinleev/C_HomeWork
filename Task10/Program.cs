b025f9ca3e239b31e0ec1ad1819f18981236ac2c
Aleksey <brinleev@example.com>
2023-04-13 13:58:45 +0300

    Task13

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая на вход принимает трехзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+int randomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число - {randomNumber}");
+Console.WriteLine($"Вторая цифра числа - {SecondDigit(randomNumber)}");
+
+int SecondDigit(int randomNum)
+{
+    int SecondDigit = randomNum % 100 / 10;
+    return SecondDigit;
+}
