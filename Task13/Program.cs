8a4252b5d8b528f81b80a64f0d5102bb7bf7ede6
Aleksey <brinleev@example.com>
2023-04-15 14:06:12 +0300

    decision Task_13_15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,3 +2,37 @@
 // которая выводит третью цифру заданного числа 
 // или сообщает,что третьей цифры нет.
 
+
+
+int ThirdDigit(int number)
+{
+while (number > 999)
+{
+    number /= 10;
+}
+return number % 10;
+}
+
+int Receipt(string message)
+{
+Console.Write(message);
+string line = Console.ReadLine();
+int result = Convert.ToInt32(line);
+return result;
+}
+
+bool InvalidDigit(int number) 
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третья цифра отсутствует");
+        return false;
+    }
+    return true;
+}
+
+int number = Receipt("Введите число - ");
+if (InvalidDigit(number))
+{
+ Console.WriteLine(ThirdDigit(number));   
+}
--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу,
+// которая принимает на вход цифру, 
+// обозначающую день недели,
+// и проверяет,
+// является отот день выходным.
+
+bool HolidayDay(int number)
+{
+    if (5 < number && number > )
+    {
+   
+return true;
+    }
+    return false;
+}
+
+bool WrongDay(int number)
+{
+    if (0 < number && number <= 7)
+    {
+  
+      return true;
+    }
+Console.WriteLine("К сожалению в неделе только семь дней");   
+    return false;
+}
+
+int Receive(string message)
+{
+Console.Write(message);
+string dayweek = Console.ReadLine();
+int result = Convert.ToInt32(dayweek);
+return result;
+}
+
+int number = Receive("Введите число - ");
+if (WrongDay(number))
+{
+ if (HolidayDay(number))
+ {
+     Console.WriteLine("УРА, это выходной");
+ }
+
+else
+{
+ Console.WriteLine("Работаем, это не выходной");    
+}
+}
+  
