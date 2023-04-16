425781660535ebbbed23ffb7c580b037cf9a937d
Aleksey <brinleev@example.com>
2023-04-16 20:52:05 +0300

    decisionTask23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу,
+// которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число =>  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Таблица кубов чисел от 1 до {n}{TableCubeNumbers(n)}");
+
+string TableCubeNumbers(int number)
+{
+    string cube = "\n";
+    for (int i = 1; i <= number; i++)
+    {
+        cube = cube + $"|{i,7} | {i*i*i,7} | \n"; 
+    }
+
+return cube;
+}
