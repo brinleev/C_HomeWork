52e44a43a0becaf76afbe2e3c3e186b832c567fa
Aleksey <brinleev@example.com>
2023-04-16 12:35:37 +0300

    Task21

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.Write("Введите пятизначное целое число =>  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = Palindrom(number);
+
+int Palindrom(int number)
+{
+int first = number / 10000;
+int second =  number / 1000 % 10;
+int fourth = number / 10 % 10;
+int fifth = number % 10;
+if (first == fifth & second  == fourth)
+{
+   Console.WriteLine("Это число является палиндромом"); 
+}
+else
+{
+   Console.WriteLine("Это число не является палиндромом");  
+}
+return first;
+}
