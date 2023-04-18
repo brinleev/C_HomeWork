4537f957c141ea7fa1ff64129dc9615d6939a519
Aleksey <brinleev@example.com>
2023-04-18 12:59:50 +0300

    decisionTask27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая принимает на вход число
+// и выдает сумму цифр в числе.
+
+
+Console.Write("Введите целое число =>  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Сумма цифр числа {n} => {SumDigits (n)}");
+
+
+int SumDigits(int n1)
+{
+    int sum = 0;
+    while (n1 > 0)
+    {
+        sum = sum + n1 % 10;
+        n1 = n1 / 10;
+    }
+    return sum;
+}
