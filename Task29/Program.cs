8d56826a1bc7d559191890232c83e8a90e52d5a6
Aleksey <brinleev@example.com>
2023-04-18 15:54:50 +0300

    decisionTask29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задает массив
+// из 8 элементов и выводит их на экран.
+
+int [] array = new int[8];
+NewArray(array);
+PrintArray(array);
+
+void NewArray(int[] userArray)
+{
+for (int i = 0; i < userArray.Length; i++)
+{
+  userArray[i] = new Random().Next(0,100);  
+}
+}
+
+void PrintArray(int[] userArray)
+{
+    Console.WriteLine("Массив из 8 элементов");
+    Console.Write("[   ");
+    for (int i = 0; i < userArray.Length; i++)
+{
+  Console.Write($"{userArray[i]}   ");
+}
+Console.Write("]");
+Console.WriteLine();
+}
