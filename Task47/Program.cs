91d8c38bf1b3a2d037f765096e48ce829776d26b
Aleksey <brinleev@example.com>
2023-04-25 16:47:52 +0300

    decisionTask47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив
+// размером m*n,
+// заполненный случайными вещественными числами.
+
+Console.Write("Введите количество строк в массиве => ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве => ");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] array2d = CreatMatrixRndDouble( line, column, -100, 100);
+Console.WriteLine();
+PrintArray(array2d);
+Console.WriteLine();
+
+double [,] CreatMatrixRndDouble(int rows, int colums, int max, int min)
+{
+    double[,] matrix = new double[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0;j < matrix.GetLength(1); j++)
+        {
+        matrix[i,j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+    return matrix;
+}
+
+void PrintArray (double [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           Console.Write ($"{Math.Round(matrix[i,j],2,MidpointRounding.ToZero),   6}  ");  
+        }
+        Console.WriteLine();
+    }
+}
