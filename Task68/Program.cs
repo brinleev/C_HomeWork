a167144dfb7495855fd1cdab6f04f63f9604964d
Aleksey <brinleev@example.com>
2023-05-02 11:19:33 +0300

    decisionTask68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -5,6 +5,19 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Функция Аккермана равно {akkerman(M, N)} ");
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
 
 
 
